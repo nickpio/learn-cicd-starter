61955c962661a61345eadeb291f0244329fb2a8e
nickpio <nickp1o@pm.me>
2026-04-05 16:15:08 -0700

    Added CD

--- main.go
+++ main.go
@@ -91,7 +91,7 @@ func main() {
 	srv := &http.Server{
 		Addr:              ":" + port,
 		Handler:           router,
-		ReadHeaderTimeout: 10,
+		ReadHeaderTimeout: 100000000000,
 	}
 
 	//log.Printf("Serving on port: %s\n", port)
